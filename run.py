0514f7fc69476da6a6be0608f3e74418b8777f06
J Klebes <fsjsk@login2.arc3.leeds.ac.uk>
2020-07-06 10:50:16 +0100

    nofield loop

--- run.py
+++ run.py
@@ -299,7 +299,7 @@ def single_run(n_steps, method = "simultaneous", field_coeffs=None, amplitude=No
       for j in range(measure_every):
         accepted = me.step_real_group()
         if accepted: se.save_temporary_matrices
-      me.measure(state) #update mean, covariance matrix, other parameters' mean by sampling this step
+      me.measure() #update mean, covariance matrix, other parameters' mean by sampling this step
   me.save_time_series()
   if outdir is not None and os.path.isdir(outdir):
     df = me.df #pd.DataFrame()
@@ -313,7 +313,7 @@ def single_run(n_steps, method = "simultaneous", field_coeffs=None, amplitude=No
   if method == "sequential":
     pickle.dump([me.real_group_sampling_width, me.complex_group_sampling_width], f)
   else:
-    pickle.dump(me.sampling_width, f)
+    pickle.dump(me.real_group_sampling_width, f)
   result_names = me.params_names
   result_names.extend(me.observables_names)
   result_means = [i for i in me.real_mean]
@@ -336,9 +336,9 @@ radius = 1
 wavenumber = .4
 
 # simulation details
-num_field_coeffs = (2,2) # z-direction modes indices go from -.. to +..; theta direction indices go from -.. to +..
+num_field_coeffs = (0,0) # z-direction modes indices go from -.. to +..; theta direction indices go from -.. to +..
 initial_sampling_width = .025
-measure_every =10
+measure_every =100
 fieldsteps_per_ampstep = 1  #nly relevant for sequential
 
 #notes = "with n = 6 - expect more of field conforming to shape.  On fixed shape a=.8." #describe motivation for a simulation here!
@@ -351,11 +351,11 @@ if __name__ == "__main__":
   notes=parser.parse_args().notes
 
   # specify type, range of plot; title of experiment
-  loop_type = ("num_field_coeffs", "fieldsteps_per_ampstep")
-  range1 = ((0,0),(1,0),(2,0), (3,0), (1,1), (2,1), (3,1))
-  range2 = range(1, 5, 10)
-  n_steps = 10#n measuring steps- so there are n_steps * measure_every amplitude steps and n_steps*measure_every*fieldsteps_per_ampsteps fieldsteps
-  method = "sequential"
+  loop_type = ("wavenumber", "kappa")
+  range1 = np.arange(0.05, 1.4, .03)
+  range2 = np.arange(0, .5, .02)
+  n_steps = 1000#n measuring steps- so there are n_steps * measure_every amplitude steps and n_steps*measure_every*fieldsteps_per_ampsteps fieldsteps
+  method = "no-field"
 
   #single_run(kappa=kappa, wavenumber=wavenumber, n_steps=n_steps, method="no-field")
 
