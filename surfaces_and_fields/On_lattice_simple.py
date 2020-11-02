49bfc3709d921936f576dad2c5b05bd0fd6e50d0
jklebes <jsklebes@googlemail.com>
2020-11-02 13:13:54 +0000

    adjusting number of lattice points for comparable spacing at different wavenumber

--- surfaces_and_fields/On_lattice_simple.py
+++ surfaces_and_fields/On_lattice_simple.py
@@ -42,7 +42,7 @@ class Lattice():
     cylinder_len_z = 2*math.pi / self.wavenumber # = wavelength lambda
     cylinder_radius_th = 2*math.pi*self.radius # circumference - in len units, not radians
     # so that z-direction pixel length is the same and results are comparable with different wavenumber
-    self.z_len = dims[0]/self.wavenumber
+    self.z_len = int(round(dims[0]/self.wavenumber))
     self.th_len = dims[1]
     self.z_pixel_len = cylinder_len_z /self.z_len # length divided py number of pixels
     assert(isclose(self.z_pixel_len,2*math.pi/dims[0])) #should be same as for wavenumber 1, length 2pi, dims[0] pixels
